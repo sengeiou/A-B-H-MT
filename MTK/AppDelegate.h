f1526cd582c2752864dd73426105b67b78503500
有限公司 深圳市 <aibaohu@youxiangongsideMac-mini.local>
2016-04-18 09:09:08 +0800

    项目初建

---
+++ MTK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTK
+//
+//  Created by 有限公司 深圳市 on 16/4/18.
+//  Copyright © 2016年 SmaLife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
